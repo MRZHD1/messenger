7a4286b4d7b81089678fc556ccdb6a7ddc14d682
Omer A <omerzahidali@gmail.com>
2023-02-08 11:46:46 +0300

    Added actioncable

---
+++ app/channels/message_channel.rb
@@ -0,0 +1,14 @@
+class MessageChannel < ApplicationCable::Channel
+  def subscribed
+    # stream_from "some_channel"
+    stream_from 'message'
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+  def receive(data)
+    ActionCable.server.broadcast('message', data)
+  end
+end
